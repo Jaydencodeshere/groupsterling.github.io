e22f8c0932a5188ef2b604a8778f6febc100a1eb
Jaydencodeshere <89849346+Jaydencodeshere@users.noreply.github.com>
2022-01-27 10:47:04 +0530

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,22 @@
+<?php
+//variable Setting
+$name = $_REQUEST['name'];
+$spouseName = $_REQUEST['spouseName'];
+$email = $_REQUEST['email'];
+$number = $_REQUEST['number'];
+$venue = $_REQUEST['venue'];
+$more = $_REQUEST['more'];
+
+//check input fields
+if (empty($name) || empty($spouseName) || empty($email) || empty($number) || empty($venue) || empty($more))
+
+{
+	echo "Please Fill All The Details";
+}
+else
+{
+	mail("groupsterling04@gmail.com", "BOOKING INFO", $message, "From: $name < $email >");
+	echo "<script type='text/javascript'>alert('your message sent succesfully');
+	window.history.log(-1);
+	</script>";
+}
